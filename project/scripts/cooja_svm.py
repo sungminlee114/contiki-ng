1fa4567df16fcecac95a52efb0536b1b1138cfa3
choiyu8 <choiyu8@yonsei.ac.kr>
2022-11-02 17:15:39 +0900

    Modified  - Plot
     Added
     - Random Forest

--- project/scripts/cooja_svm.py
+++ project/scripts/cooja_svm.py
@@ -9,9 +9,10 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as pl
 from itertools import cycle
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input', type=str)
+parser.add_argument('input', type=str, nargs="+")
 
 conopts = parser.parse_args()
 import sys
@@ -20,7 +21,8 @@ if sys.platform.startswith("win") and "*" in conopts.input:
 
 # Read data
 
-if conopts.input.endswith(".csv"):
+if len(conopts.input) == 1 and conopts.input[0].endswith(".csv"):
+    conopts.input = conopts.input[0]
     assert (os.path.exists(conopts.input))
     df_data = pd.read_csv(conopts.input)
     print("Read preprocessed data fin")
@@ -76,15 +78,19 @@ else:
         all_data.append(pd.concat(datas))
 
     df_data = pd.concat(all_data)
-    df_data.to_csv(f"{simul_dir}/../data-ws{window_size}-dp{detect_period}-asi{attack_standard_idx}.csv")
+    csv_path = f"{simul_dir}/../data-ws{window_size}-dp{detect_period}-asi{attack_standard_idx}.csv"
+    df_data.to_csv(csv_path)
 
     print("Data process fin")
+
+    conopts.input = csv_path
 
 
 # -- filter cols
 feature_cols = [
     # 'Time', 
-    'Mote', 'Seq', 'Rank',
+    'Mote', 
+    # 'Seq', 'Rank',
     'DIS-R', 'DIS-S', 'DIO-R', 'DIO-S', 'DAO-R',
     # 'RPL-total-sent'
     ]
@@ -95,16 +101,17 @@ for c in df_data.columns:
             cols.append(c)
 
 # -- filter rows
-df_data = df_data[df_data['Trxr'] == 0.7]
-df_data = df_data[np.isin(df_data['Attack'], ['No', "dio-drop"])]
+trxrs = [.7, 0.8, 0.9, 1.0]
+df_data = df_data[np.isin(df_data['Trxr'], trxrs)]
+# df_data = df_data[np.isin(df_data['Attack'], ['No', "dio-drop"])]
 
 # -- get dasta
 
 X = df_data[cols]
 Y = df_data['Attack']
-
+print(cols, X)
 # -- PCA
-pca_n_comp = 2
+pca_n_comp =   2 # pca dimension (2 or 3)
 pca = PCA(n_components=pca_n_comp, whiten=True).fit(X)
 X_pca = pca.transform(X)
 print('explained variance ratio:',pca.explained_variance_ratio_)
@@ -114,29 +121,54 @@ principalDf = pd.DataFrame(data = X_pca, columns = pcacol)
 
 colors = cycle('rgb')
 target_names = np.unique(Y)
+if True:
+    fg = pl.figure(figsize=(16, 9))
+    for fi, trxr in enumerate(trxrs):
+        print(fi, trxr)
+        ax = fg.add_subplot(2, 2, fi + 1)
 
-fg = pl.figure()
-ax = fg.add_subplot(projection='3d') if pca_n_comp == 3 else fg.add_subplot()
+        tix = df_data['Trxr'] == trxr
+        target_list = np.array(Y[tix]).flatten()
+        _X_pca = X_pca[tix]
+        for t_name, c in zip(target_names, colors):
+                ax.scatter(_X_pca[target_list == t_name, 0], _X_pca[target_list == t_name,1],
+                c=c, label=t_name, s=2)
+        ax.legend(target_names)
+        ax.set_xlabel('pca 1')
+        ax.set_ylabel('pca 2')
+        ax.set_title(trxr)
+        
+else:
+    fg = pl.figure()
+    ax = fg.add_subplot(projection='3d') if pca_n_comp == 3 else fg.add_subplot()
 
-target_list = np.array(Y).flatten()
-for t_name, c in zip(target_names, colors):
-        if pca_n_comp == 2:
-            ax.scatter(X_pca[target_list == t_name, 0], X_pca[target_list == t_name,1],
-            c=c, label=t_name, s=2)
-        elif pca_n_comp == 3:
-            ax.scatter(X_pca[target_list == t_name, 0], X_pca[target_list == t_name,1], 
-                    X_pca[target_list == t_name, 2], c=c, label=t_name)
-ax.legend(target_names)
-ax.set_xlabel('pca 1')
-ax.set_ylabel('pca 2')
-if pca_n_comp == 3:
-    ax.set_zlabel('pca 3')
-pl.show()
+    target_list = np.array(Y).flatten()
+    for t_name, c in zip(target_names, colors):
+            if pca_n_comp == 2:
+                ax.scatter(X_pca[target_list == t_name, 0], X_pca[target_list == t_name,1],
+                c=c, label=t_name, s=2)
+            elif pca_n_comp == 3:
+                ax.scatter(X_pca[target_list == t_name, 0], X_pca[target_list == t_name,1], 
+                        X_pca[target_list == t_name, 2], c=c, label=t_name)
+    ax.legend(target_names)
+    ax.set_xlabel('pca 1')
+    ax.set_ylabel('pca 2')
+    if pca_n_comp == 3:
+        ax.set_zlabel('pca 3')
 
+# pl.show()
+pl.tight_layout()
+pl.savefig(conopts.input.replace(".csv", ".png"))
 # -- run model
 
-# clf = svm.SVC(kernel='rbf', C=1, verbose=True, shrinking=0)
-# scores_res = model_selection.cross_val_score(clf, X, Y,cv=5)
+clf = svm.SVC(kernel='rbf', C=1, verbose=True, shrinking=0)
+scores_res = model_selection.cross_val_score(clf, X, Y,cv=5)
 
-# print(scores_res)
-# print(scores_res.mean())
+print(scores_res)
+print(scores_res.mean())
+
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+scores_res = model_selection.cross_val_score(model, X, Y,cv=5)
+
+print(scores_res)
+print(scores_res.mean())
